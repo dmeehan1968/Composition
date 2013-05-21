e2ea7b3dd6f51ecb2f684bf5b0c99348262d3b89
Dave Meehan <dave_meehan@replicated.co.uk>
2013-05-21 20:08:33 +0100

    Updated unit tests for MutableArray to test different types of input array

--- Composition/CompositionTests/MutableArraySpec.m
+++ Composition/CompositionTests/MutableArraySpec.m
@@ -46,11 +46,50 @@ describe(@"Mutable Array", ^{
             [[sut should] haveCountOf:0];
             
         });
+        
+    });
 
-
+    context(@"with a non-empty, immutable array as input", ^{
+        
+        beforeEach(^{
+            
+            sut = [[MutableArray alloc] initWithArray:@[ @"One", @"Two" ]];
+            
+        });
+        
+        it(@"should be possible to remove one object leaving one", ^{
+            
+            [sut removeObject: @"Two"];
+            
+            [[sut should] haveCountOf:1];
+            
+            [[[sut objectAtIndex:0] should] equal:@"One"];
+            
+        });
         
     });
     
+    context(@"with a non-empty, mutable array as input", ^{
+        
+        beforeEach(^{
+            
+            sut = [[MutableArray alloc] initWithArray:[@[ @"One", @"Two" ] mutableCopy]];
+            
+        });
+        
+        it(@"should be possible to remove one object leaving one", ^{
+            
+            [sut removeObject: @"Two"];
+            
+            [[sut should] haveCountOf:1];
+            
+            [[[sut objectAtIndex:0] should] equal:@"One"];
+
+        });
+        
+    });
+    
+
 
 });
 
